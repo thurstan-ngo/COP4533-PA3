747567e2ca605ce9c354a5ac8393a37b527720c4
thurstanngo <146325171+thurstan-ngo@users.noreply.github.com>
2026-04-06 18:38:18 -0400

    Compute max value of a common subsequence and reconstruct one optimal subsequence

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 import sys
 
+def hvlcs(A, B, value_dict):
+    m, n = len(A), len(B)
+    if m == 0 or n == 0:
+        return 0
+    
+    # Build table
+    arr = [[0]*(n+1) for _ in range(m+1)]
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if A[i-1] == B[j-1]:
+                arr[i][j] = arr[i-1][j-1] + value_dict[A[i-1]]
+            else:
+                arr[i][j] = max(arr[i-1][j], arr[i][j-1])
+
+    # Backtracking
+    i, j = m, n
+    subsequence = []
+    while i > 0 and j > 0:
+        if A[i-1] == B[j-1]:
+            subsequence.append(A[i-1])
+            i -= 1
+            j -= 1
+        elif arr[i-1][j] >= arr[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+    
+    subsequence.reverse()
+    return arr[m][n], "".join(subsequence)
+    
+
 def main():
     input_file = sys.argv[1]
 
@@ -18,6 +50,11 @@ def main():
         # Read the two strings
         first_str = file.readline()
         second_str = file.readline()
+    
+    # Run the dynamic programming algorithm and output results
+    max_value, subsequence = hvlcs(first_str, second_str, value_dict)
+    print(max_value)
+    print(subsequence)
     
 
 if __name__ == '__main__':
